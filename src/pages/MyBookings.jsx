4b9c571610532341343ed451a86977bebc1103f1
Sumant Kumar pandit <sumantpandit993127@gmail.com>
2025-12-31 22:28:07 +0530

    first commit

--- src/pages/MyBookings.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyBookings = () => {
-  return (
-    <div>MyBookings</div>
-  )
-}
-
-export default MyBookings
--- src/pages/SeatLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SeatLayout = () => {
-  return (
-    <div>SeatLayout</div>
-  )
-}
-
-export default SeatLayout
